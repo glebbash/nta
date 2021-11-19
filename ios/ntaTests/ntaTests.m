c940f38b62593ad10a35ba025d7607228d99327e
Gleb Bashkatov <glebbash@gmail.com>
2021-11-19 22:19:33 +0200

    feat: rename to nta

--- ios/ntaTests/ntaTests.m
+++ ios/ntaTests/ntaTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface rnStarterTests : XCTestCase
+@interface ntaTests : XCTestCase
 
 @end
 
-@implementation rnStarterTests
+@implementation ntaTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
