584df2b09a7fe860b660be8f4108c0b1ed7480b3
glebbash <glebbash@gmail.com>
2022-11-13 21:17:12 +0000

    feat: add serCurrent query option

--- client/src/components/Box.js
+++ client/src/components/Box.js
@@ -1,9 +1,12 @@
 import { html } from "../deps.js";
 import { createComponent } from "../utils/create-component.js";
 
-export const Row = ({ data: { content } }) => {
+export const Box = ({ data: { content, align } }) => {
   return html`
-    <div style=${{ display: "flex" }}>
+    <div style=${{
+    display: "flex",
+    ...(align === "vertical" && { flexDirection: "column" }),
+  }}>
       ${content.map(createComponent)}
     </div>
   `;
