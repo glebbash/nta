0ce94e490c75b67f899ea1707239ce9bad5654a1
glebbash <glebbash@gmail.com>
2023-01-13 14:16:01 +0000

    chore: remove unused code

--- client/src/utils/get-page-title.ts
+++
@@ -1,9 +0,0 @@
-import { PageData } from "../hooks/usePagePersistence";
-
-export function getPageTitle(meta: PageData["meta"]) {
-  if (typeof meta.title !== "string") {
-    return "New Page";
-  }
-
-  return meta.title;
-}
